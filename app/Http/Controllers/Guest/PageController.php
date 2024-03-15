a5134f7ed03945a4a4a75430cf3c799316353f2c
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-03-15 17:52:44 +0100

    added query

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\train;
+use Illuminate\Support\Carbon;
 
 class PageController extends Controller
 {
     //
     public function index(){
-        $train=train::all();
+        $train=train::where('Orario_di_Partenza', '>', Carbon::now())->get();
         return view("layouts.app" ,compact('train'));
     }
 }
